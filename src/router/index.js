a230ed9884a792e73d0abef415aaf5b908d172b1
Sadeen2 <sadeenryahi66@gmail.com>
2025-10-10 20:49:15 +0300

    Switch to hash mode in Vue Router to fix History page on GitHub Pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomePage from '../pages/HomePage.vue'
 import WhoWeAre from '../pages/WhoWeAre.vue'
 import History from '../pages/History.vue'
@@ -11,7 +11,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory('/The-Jerusalem-Fund/'),
+  history: createWebHashHistory('/The-Jerusalem-Fund/'),
   routes
 })
 
